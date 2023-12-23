0ec92bfb8fc2676fcb7b9e425c9518a00dd296be
Mohammed Yussif <mohammed.yussif@invennico.com>
2023-12-23 19:01:05 +0000

    init project

---
+++ android/app/src/main/kotlin/coded/studio/com/x_clone_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package coded.studio.com.x_clone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants {
+  static const String dataBaseId = "65872b95266d7b26f923";
+  static const String projectId = "6587292e45f9e41230f3";
+
+  static const String endpoint = "https://localhost:80/v1";
+}
