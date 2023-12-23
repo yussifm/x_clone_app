11a184a5b8af2e83d234fe32649484c901e10e72
Mohammed Yussif <mohammed.yussif@invennico.com>
2023-12-23 19:22:42 +0000

    .

---
+++ lib/const/resource.dart
@@ -0,0 +1,55 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class AssetsConstants {
+  const AssetsConstants._();
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\comment.svg)
+  static const String ASSETS_SVG_COMMENT_SVG = 'assets/svg/comment.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\emoji.svg)
+  static const String ASSETS_SVG_EMOJI_SVG = 'assets/svg/emoji.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\gallery.svg)
+  static const String ASSETS_SVG_GALLERY_SVG = 'assets/svg/gallery.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\gif.svg)
+  static const String ASSETS_SVG_GIF_SVG = 'assets/svg/gif.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\home_filled.svg)
+  static const String ASSETS_SVG_HOME_FILLED_SVG = 'assets/svg/home_filled.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\home_outlined.svg)
+  static const String ASSETS_SVG_HOME_OUTLINED_SVG =
+      'assets/svg/home_outlined.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\like_filled.svg)
+  static const String ASSETS_SVG_LIKE_FILLED_SVG = 'assets/svg/like_filled.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\like_outlined.svg)
+  static const String ASSETS_SVG_LIKE_OUTLINED_SVG =
+      'assets/svg/like_outlined.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\notif_filled.svg)
+  static const String ASSETS_SVG_NOTIF_FILLED_SVG =
+      'assets/svg/notif_filled.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\notif_outlined.svg)
+  static const String ASSETS_SVG_NOTIF_OUTLINED_SVG =
+      'assets/svg/notif_outlined.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\retweet.svg)
+  static const String ASSETS_SVG_RETWEET_SVG = 'assets/svg/retweet.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\search.svg)
+  static const String ASSETS_SVG_SEARCH_SVG = 'assets/svg/search.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\verified.svg)
+  static const String ASSETS_SVG_VERIFIED_SVG = 'assets/svg/verified.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\views.svg)
+  static const String ASSETS_SVG_VIEWS_SVG = 'assets/svg/views.svg';
+
+  /// ![preview](file://D:\programming\pratice_area\Dart\x_clone_app\assets\svg\x.svg)
+  static const String ASSETS_SVG_X_SVG = 'assets/svg/x.svg';
+}
